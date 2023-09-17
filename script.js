7dfed923c95f7bfbfddfdeb4f489d5e645c2efb4
Eliv4389 <vidaleli1989@icloud.com>
2023-09-17 00:15:02 -0300

    final

--- script.js
+++ script.js
@@ -8,96 +8,27 @@ function showSmileyAndNavigate(url) {
   }, 1000); // 1000 milliseconds = 1 second
 }
 
+function addRecommendation() {
+  let recommendation = document.getElementById("new_recommendation");
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
 
-function changeBackground() {
-  document.body.style.backgroundColor = "lightblue"; // Or any color you like
-}
+    // Call showPopup here
+    showPopup(true);
 
-function playAudio() {
-  const audio = document.querySelector("audio");
-  audio.play();
-}
+    var element = document.createElement("div");
+    element.setAttribute("class", "recommendation");
+    element.innerHTML = '<span>&#8220;</span>' + recommendation.value + '<span>&#8221;</span>';
+    document.getElementById("all_recommendations").appendChild(element);
 
-function pauseAudio() {
-  const audio = document.querySelector("audio");
-  audio.pause();
-}
-
-let text = "Hi 👋, I'm Eli";
-let index = 0;
-let targetElement = document.getElementById('title');
-let newText = "";
-
-function animateText() {
-  if (index < text.length) {
-    let codePoint = text.codePointAt(index);
-
-    // Add the character to newText
-    newText += String.fromCodePoint(codePoint);
-    
-    // Update the innerText of the target element
-    targetElement.innerText = newText;
-
-    // If the code point is above 0xFFFF, we are dealing with a surrogate pair.
-    // Skip the next index since we've already processed both parts of the surrogate pair.
-    if (codePoint > 0xFFFF) {
-      index += 2;
-    } else {
-      index++;
-    }
-
-    setTimeout(animateText, 300); // 300ms delay between characters
+    recommendation.value = "";
   }
 }
 
-window.onload = function() {
-  animateText();
-};
-
-
-function toggleDescription() {
-  const para = document.querySelector("p");
-  if (para.style.display === "none" || para.style.display === "") {
-    para.style.display = "block";
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
   } else {
-    para.style.display = "none";
+    document.getElementById('popup').style.visibility = 'hidden'
   }
 }
-
-document.querySelectorAll("tr").forEach(row => {
-  row.addEventListener("mouseover", function() {
-    // Show tooltip
-  });
-  row.addEventListener("mouseout", function() {
-    // Hide tooltip
-  });
-});
-
-function startTimer() {
-  // Your countdown logic here
-}
-
-let currentImage = 0;
-function nextImage() {
-  // Show next image in the array
-}
-
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Make sure the DOM is fully loaded
-  let countdownElement = document.getElementById("countdown");
-  let countdownValue = 10;
-
-  document.getElementById("startCountdown").addEventListener("click", function() {
-    let intervalId = setInterval(function() {
-      countdownElement.textContent = countdownValue;
-
-      if (countdownValue <= 0) {
-        clearInterval(intervalId);
-        countdownElement.textContent = "Time's up!";
-      }
-
-      countdownValue -= 1;
-    }, 1000);
-  });
-});
